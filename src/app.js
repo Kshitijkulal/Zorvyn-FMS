224c4fea24acc98148e258e120cea62c70ad3666
Kshitij Kulal <121379095+Kshitijkulal@users.noreply.github.com>
2026-04-03 02:46:34 +0530

    Global and strict auth rate limiter added

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ import helmet from "helmet";
 import routes from "./routes/index.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 
+import { apiLimiter } from "./middlewares/rateLimit.middleware.js"
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
+app.use("/api", apiLimiter)
 app.use(`/api`,routes);
 
 app.use(errorMiddleware)
---
+++ src/middlewares/rateLimit.middleware.js
@@ -0,0 +1,25 @@
+import rateLimit from "express-rate-limit"
+
+// 🔹 Global limiter
+export const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 mins
+  max: 100, // max requests per IP
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests, please try again later"
+  }
+})
+
+// 🔹 Strict limiter (for auth)
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many login attempts, please try again later"
+  }
+})
