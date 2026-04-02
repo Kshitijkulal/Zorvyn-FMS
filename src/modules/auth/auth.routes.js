c592e54d4762635fb078f69b9c956e5d16613082
Kshitij Kulal <kshitij@Kshitijs-MacBook-Air.local>
2026-04-03 02:46:34 +0530

    Global and strict auth rate limiter added

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -9,15 +9,16 @@ import {
 } from "./auth.validation.js"
 import { validate } from "../../middlewares/validate.middleware.js"
 import { authenticate } from "../../middlewares/auth.middleware.js"
+import { authLimiter } from "../../middlewares/rateLimit.middleware.js"
 
 
 const router = Router()
 
-router.post("/register", validate(registerSchema), authController.register)
-router.post("/verify-otp", validate(verifyOtpSchema), authController.verifyOtp)
-router.post("/login", validate(loginSchema), authController.login)
-router.post("/forgot-password", validate(forgotPasswordSchema), authController.forgotPassword)
-router.post("/reset-password", validate(resetPasswordSchema), authController.resetPassword)
+router.post("/register", validate(registerSchema),authLimiter, authController.register)
+router.post("/verify-otp", validate(verifyOtpSchema), authLimiter, authController.verifyOtp)
+router.post("/login", validate(loginSchema), authLimiter, authController.login)
+router.post("/forgot-password", validate(forgotPasswordSchema), authLimiter, authController.forgotPassword)
+router.post("/reset-password", validate(resetPasswordSchema), authLimiter, authController.resetPassword)
 router.post("/logout", authenticate, authController.logout)
 
 export default router
